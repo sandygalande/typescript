4dd66b31bc3544086d97c4c3a6177162efa89e26
sandygalande <sandipgalande0076@gmail.com>
2024-02-16 14:07:14 +0530

    update
    class with constructor and remove duplicate element from array

--- arrayAssgn.js
+++ arrayAssgn.js
@@ -1,3 +1,23 @@
+console.log("<===========Class with Constructor================>");
+var Student = /** @class */ (function () {
+    function Student(id, name, city, marks) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+        this.marks = marks;
+    }
+    Student.prototype.getDetails = function () {
+        console.log("ID: ".concat(this.id, ", Name: ").concat(this.name, ", City: ").concat(this.city, ", Marks: ").concat(this.marks));
+    };
+    return Student;
+}());
+var stew = new Student(1, "Stew", "Pune", 75);
+stew.getDetails();
+var elon = new Student(2, "Elon", "Nashik", 60);
+elon.getDetails();
+var jenny = new Student(3, "Jenny", "Mumbai", 80);
+jenny.getDetails();
+console.log("<=====Remove Duplicate Element from array======>");
 var inputArr = [1, 2, 2, 4, 5, 4, 7, 8, 7, 3, 6];
 var uniqueArr = [];
 for (var i = 0; i < inputArr.length; i++) {
--- arrayAssgn.ts
+++ arrayAssgn.ts
@@ -1,4 +1,30 @@
 
+console.log("<===========Class with Constructor================>");
+class Student{
+    id:number;
+    name:string;
+    city:string;
+    marks:number;
+
+    constructor(id:number, name: string, city:string, marks:number){
+        this.id = id;
+        this.name =name;
+        this.city =city;
+        this.marks =marks;
+    }
+    getDetails(): void{
+        console.log(`ID: ${this.id}, Name: ${this.name}, City: ${this.city}, Marks: ${this.marks}`)
+    }
+}
+const stew: Student = new Student(1,"Stew","Pune",75);
+stew.getDetails();
+const elon: Student = new Student(2,"Elon","Nashik",60);
+elon.getDetails();
+const jenny: Student = new Student(3,"Jenny","Mumbai",80);
+jenny.getDetails();
+
+
+console.log("<=====Remove Duplicate Element from array======>");
 
 var inputArr: number[] = [1, 2, 2, 4, 5, 4, 7, 8, 7, 3, 6];
 var uniqueArr: number[] =[];
